8f25bca539af8f5e02e4b442756aacef8fea8e4a
Deftnt Nguyen <jimmi2051@gmail.com>
2019-11-24 17:14:53 +0700

    [Update] Wrong Url Video loading

--- src/h5p/VideoNormal/index.js
+++ src/h5p/VideoNormal/index.js
@@ -2,11 +2,19 @@ import React, { Component } from "react";
 import { Player } from "video-react";
 const REACT_APP_URL_API = process.env.REACT_APP_URL_API;
 class VideoNormal extends Component {
+  processUrl = url => {
+    let regexUrl = /^http/
+    if (regexUrl.test(url)) {
+      return url;
+    }
+    return `${REACT_APP_URL_API}${url}`;
+  }
   render() {
+    const currentUrl = this.processUrl(this.props.src)
     return (
       <div className="content-video-normal">
         <h5 className="content-video-normal__title">Content: </h5>
-        <Player playsInline src={`${REACT_APP_URL_API}${this.props.src}`} />
+        <Player playsInline src={currentUrl} />
       </div>
     );
   }
